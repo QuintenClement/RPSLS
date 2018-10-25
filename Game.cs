13dea4ef5d2722215715d02761deb0e592c12434
Quinten Clement <quintenclement@gmail.com>
2018-10-25 13:40:39 -0500

    forgot to save before last commit oops

--- Game.cs
+++ Game.cs
@@ -8,5 +8,44 @@ namespace TheRealRPSLS
 {
     class Game
     {
+        // member variables (HAS A)
+        public Player playerOne;
+        public Player playerTwo;
+
+        public Game()
+        {
+            playerOne = new Human();
+        }
+
+        public void StartGame()
+        {
+            // display instructions
+            // ask what type of game
+            // choose gestures
+            playerOne.ChooseGesture();
+            playerTwo.ChooseGesture();
+        }
+
+        public void SelectGameType()
+        {
+            Console.WriteLine("Would you like to play against another person or against a computer? (person or computer)");
+            string Gametype = Console.ReadLine();
+            if (Gametype.Equals("person") || Gametype.Equals("Person"))
+            {
+                Console.WriteLine("You chose to play against another person.");
+                playerTwo = new Human();
+            }
+            else if (Gametype.Equals("computer") || Gametype.Equals("Computer"))
+            {
+                Console.WriteLine("You chose to play against a computer.");
+                playerTwo = new Computer();
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid response.");
+                SelectGameType();
+            }
+        }
+
     }
 }
