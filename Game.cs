3a0db3202511c9286b4cd2586bac4f70b24fa130
Quinten Clement <quintenclement@gmail.com>
2018-10-25 15:24:31 -0500

    added switch case to computer class. added who wins method. gave the game some context

--- Game.cs
+++ Game.cs
@@ -20,8 +20,17 @@ namespace TheRealRPSLS
         public void StartGame()
         {
             // display instructions
+            Console.WriteLine("Hello! And welcome to the first ever RPSLS Championship!");
+            Console.WriteLine("My my, you look like one hell of a challenger. Do you have what it takes to become the next Champion?");
+            Console.WriteLine("The rules are simple: scissors cuts paper, paper covers rock, rock crushers lizard, lizard poisens spock, spock smashes scissors, scissors decapitates lizard,);" +
+                "lizard eats paper, paper disproves spock, spock vaporizes rock, and as it always has rock crushes scissors");
+            Console.WriteLine("Got it?");
+            Console.WriteLine("Good!");
             // ask what type of game
+            SelectGameType();
             // choose gestures
+            Console.ReadLine();
+
             playerOne.ChooseGesture();
             playerTwo.ChooseGesture();
         }
@@ -29,7 +38,7 @@ namespace TheRealRPSLS
         public void SelectGameType()
         {
             Console.WriteLine("Would you like to play against another person or against a computer? (person or computer)");
-            string Gametype = Console.ReadLine();
+            string Gametype = Console.ReadLine().ToLower();
             if (Gametype.Equals("person") || Gametype.Equals("Person"))
             {
                 Console.WriteLine("You chose to play against another person.");
@@ -47,5 +56,63 @@ namespace TheRealRPSLS
             }
         }
 
+        public void WhoWins()
+        {
+            if (playerOne.gesture == "rock" && playerTwo.gesture == "paper" || playerTwo.gesture == "spock")
+            {
+                playerTwo.score++;
+                Console.WriteLine("Player 2 won! Better luck next time Player 1");
+            }
+
+            else if (playerOne.gesture == "paper" && playerTwo.gesture == "scissors" || playerTwo.gesture == "lizard")
+            {
+                playerTwo.score++;
+                Console.WriteLine("Player 2 won! Better luck next time Player 1");
+            }
+
+            else if (playerOne.gesture == "scissors" && playerTwo.gesture == "rock" || playerTwo.gesture == "spock")
+            {
+                playerTwo.score++;
+                Console.WriteLine("Player 2 won! Better luck next time Player 1");
+            }
+
+            else if (playerOne.gesture == "lizard" && playerTwo.gesture == "scissors" || playerTwo.gesture == "rock")
+            {
+                playerTwo.score++;
+                Console.WriteLine("Player 2 won! Better luck next time Player 1");
+            }
+
+            else if (playerOne.gesture == "spock" && playerTwo.gesture == "paper" || playerTwo.gesture == "lizard")
+            {
+                playerTwo.score++;
+                Console.WriteLine("Player 2 won! Better luck next time Player 1");
+            }
+
+            else if (playerOne.gesture == "spock" && playerTwo.gesture == "rock" || playerTwo.gesture == "scissors")
+            {
+                playerOne.score++;
+                Console.WriteLine("Player 1 won! Better luck next time Player 2");
+            }
+            else if (playerOne.gesture == "lizard" && playerTwo.gesture == "paper" || playerTwo.gesture == "spock")
+            {
+                playerOne.score++;
+                Console.WriteLine("Player 1 won! Better luck next time Player 2");
+            }
+            else if (playerOne.gesture == "scissors" && playerTwo.gesture == "paper" || playerTwo.gesture == "lizard")
+            {
+                playerOne.score++;
+                Console.WriteLine("Player 1 won! Better luck next time Player 2");
+            }
+            else if (playerOne.gesture == "paper" && playerTwo.gesture == "rock" || playerTwo.gesture == "spock")
+            {
+                playerOne.score++;
+                Console.WriteLine("Player 1 won! Better luck next time Player 2");
+            }
+            else if (playerOne.gesture == "rock" && playerTwo.gesture == "scissors" || playerTwo.gesture == "lizard")
+            {
+                playerOne.score++;
+                Console.WriteLine("Player 1 won! Better luck next time Player 2");
+            }
+        }
     }
 }
